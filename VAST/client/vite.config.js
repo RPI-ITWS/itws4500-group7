b411ff22bb2fe69f7f0e277b13786d4a3ac792a0
carsoe2 <carsoe2@rpi.edu>
2023-04-25 14:19:34 -0400

    Revert "ok"
    This reverts commit b92af43968e53821143f1015bd10aef606a74dfb.

--- VAST/client/vite.config.js
+++ VAST/client/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-});
+  root: './', // This points to your project root directory
+  base: '/', // The base path for your application; typically, this is '/'
+  publicDir: 'build/public', // This tells Vite to use the index.html inside the build/public folder
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [reactRefresh()],
+})
