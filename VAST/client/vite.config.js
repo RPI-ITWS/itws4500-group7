ef076314bd7c6fde59309f6f09525f81e37b7b8b
ddortirossi <dannydoritos123@gmail.com>
2023-04-25 13:13:00 -0400

    updating

--- VAST/client/vite.config.js
+++ VAST/client/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  root: './', // This points to your project root directory
+  base: '/', // The base path for your application; typically, this is '/'
+  publicDir: 'build/public', // This tells Vite to use the index.html inside the build/public folder
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+  },
+  plugins: [reactRefresh()],
 })
