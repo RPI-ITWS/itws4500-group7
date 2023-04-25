01feaa6f8930f565bf15a3e08bfb23ac9f2055df
carsoe2 <carsoe2@rpi.edu>
2023-04-25 14:19:45 -0400

    Revert "updating"
    This reverts commit ef076314bd7c6fde59309f6f09525f81e37b7b8b.

--- VAST/client/vite.config.js
+++ VAST/client/vite.config.js
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  root: './', // This points to your project root directory
-  base: '/', // The base path for your application; typically, this is '/'
-  publicDir: 'build/public', // This tells Vite to use the index.html inside the build/public folder
-  build: {
-    outDir: 'dist',
-  },
   plugins: [reactRefresh()],
+  build: {
+    outDir: 'dist'
+  }
 })
